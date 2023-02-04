c4f61d3bad8f8d1b8d4ab8f39711505a41b9d503
Ivan Diaz <ivandiaz94ing@gmail.com>
2023-02-04 10:48:55 -0500

    Seccion 2 Introduccion a JavaScript

---
+++ 01Fundamentos/assets/js/alert.js
@@ -0,0 +1,8 @@
+
+alert('Hola Mundo');
+
+let nombre = prompt('Cual es tu nombre?');
+console.log(nombre);
+
+const confirma = confirm('Deseas borrar el archivo?');
+console.log(confirma);
---
+++ 01Fundamentos/assets/js/app.js
@@ -0,0 +1,13 @@
+alert('Hola mundo desde app.js');
+
+let a = 10,
+     b =  20,
+     c = 'Hola ',
+     d = 'Spiderman',
+     x = a + b;
+
+const saludo = c + d;
+
+//console.log(saludo);
+
+let miNombre = 'Ivan';
